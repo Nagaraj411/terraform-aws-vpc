24e4e639bbb9785c3d6f06ffcea4793992fba5df
Nagaraj <nagarajuande255@gmail.com>
2025-06-19 09:31:58 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 # creating a VPC with a  CIDR block and enabling DNS hostnames
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main" {  # this lines 3-14 create a VPC
   cidr_block       = var.cidr_block
   instance_tenancy = "default"
   enable_dns_hostnames = "true"
@@ -12,3 +12,14 @@ resource "aws_vpc" "main" {
     }
   )
 }
+
+resource "aws_internet_gateway" "main" {  # this lines 16-22 create an Internet Gateway
+  vpc_id = aws_vpc.main.id  # automatically attach the Internet Gateway to the VPC
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}" # Name of the Internet Gateway roboshop-dev
+    }
+  )
+} 
